0503303f0fe255c0f2ba592272bac3d0581dfc02
Pouya Akbarzadeh <pouya.akbar@gmail.com>
2024-08-17 15:11:14 -0500

    Wifi fix, added some more info for debugging
    Wifi was never broken, but now there is more debugging info is added.
    
    Need to update wifi issue in notes so others know

--- sketch_aug15a/sketch_aug15a.ino
+++ sketch_aug15a/sketch_aug15a.ino
@@ -10,27 +10,57 @@ Adafruit_BMP280 bmp; // I2C
 
 void setup() {
   Serial.begin(115200);
+
+  delay(1000);
+  
+  //Scan for Wifi
+  Serial.println("Scanning for WiFi networks...");
+  int n = WiFi.scanNetworks();
+  if (n == 0) 
+  {
+    Serial.println("No networks found");
+  } 
+  else
+  {
+    Serial.println("Networks found:");
+    for (int i = 0; i < n; ++i) 
+    {
+      Serial.printf("%d: %s, Signal strength (RSSI): %d dBm\n", i + 1, WiFi.SSID(i).c_str(), WiFi.RSSI(i));
+      delay(10);
+      if(WiFi.SSID(i) == ssid)
+      {
+        Serial.println(ssid);
+        Serial.println(WiFi.SSID(i));
+        Serial.println("Your network is in range");
+        Serial.println("Connecting to WiFi...");
+        WiFi.begin(ssid, password);
+        // Wait for connection
+        while (WiFi.status() != WL_CONNECTED)
+        {
+          delay(1000);
+          Serial.print(".");
+        }
+        if(WiFi.status() == WL_CONNECTED)
+        {
+          // Print the IP address
+          Serial.println();
+          Serial.println("Connected to WiFi");
+          Serial.print("IP Address: ");
+          Serial.println(WiFi.localIP());
+        }
+      }
+      break;
+    }
+  }
+
+  delay(100);
+
+
   if (!bmp.begin(0x76)) {
     Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
     while (1);
   }
 
-  // Connect to Wi-Fi
-  Serial.println();
-  Serial.println("Connecting to WiFi...");
-  WiFi.begin(ssid, password);
-
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
-  }
-
-  // Print the IP address
-  Serial.println();
-  Serial.println("Connected to WiFi");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
 
 }
 
